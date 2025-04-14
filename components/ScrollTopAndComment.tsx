113a703272a260045f9acd7eaea9a50a797ded49
Ketan Shukla <ketankshukla@gmail.com>
2025-04-13 21:43:41 -0700

    Changed scroll-to-comment icon from chat to down arrow

--- components/ScrollTopAndComment.tsx
+++ components/ScrollTopAndComment.tsx
@@ -56,7 +56,7 @@ const ScrollTopAndComment = () => {
           <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path
               fillRule="evenodd"
-              d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+              d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
               clipRule="evenodd"
             />
           </svg>
