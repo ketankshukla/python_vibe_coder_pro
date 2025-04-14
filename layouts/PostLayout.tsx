3bf9d76ee6e56e8a584af596579f3d990f64ef1f
Ketan Shukla <ketankshukla@gmail.com>
2025-04-13 22:45:04 -0700

    sidebar from blog article page

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -42,45 +42,8 @@ export default function PostLayout({ content, authorDetails, children }: LayoutP
               </div>
             </div>
           </header>
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 md:grid md:grid-cols-4 md:gap-x-6 md:divide-y-0">
-            <dl className="pb-10 pt-6 md:border-b md:border-gray-200 md:pt-11 md:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 md:block md:space-x-0 md:space-y-8">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Image
-                          src={author.avatar}
-                          width={38}
-                          height={38}
-                          alt="avatar"
-                          className="h-10 w-10 rounded-full"
-                        />
-                      )}
-                      <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                        <dt className="sr-only">Name</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter
-                                .replace('https://twitter.com/', '@')
-                                .replace('https://x.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
-            </dl>
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 md:col-span-3 md:row-span-2 md:pb-0">
+          <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700">
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 w-full">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               {siteMetadata.comments && (
                 <div
@@ -91,22 +54,19 @@ export default function PostLayout({ content, authorDetails, children }: LayoutP
                 </div>
               )}
             </div>
-            <footer>
-              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 md:col-start-1 md:row-start-2 md:divide-y">
-                {tags && tags.length > 0 && (
-                  <div className="py-4 md:py-8">
-                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                      Tags
-                    </h2>
-                    <div className="flex flex-wrap justify-center gap-2 pt-4 md:justify-start">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
-                    </div>
+            <footer className="py-4">
+              {tags && tags.length > 0 && (
+                <div className="py-4">
+                  <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                    Tags
+                  </h2>
+                  <div className="flex flex-wrap gap-2 pt-4">
+                    {tags.map((tag) => (
+                      <Tag key={tag} text={tag} />
+                    ))}
                   </div>
-                )}
-
-              </div>
+                </div>
+              )}
             </footer>
           </div>
         </div>
